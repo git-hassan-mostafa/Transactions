c0cacc5367561c22438d2b571b33976ec09ad0d3
git-hassan-mostafa <mohamad.hm464@gmail.com>
2024-07-24 23:37:39 +0300

    updating app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,52 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ModelsModule } from './Models/models.module';
+import { CustomersController } from './controllers/customers/customers.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
+import { CustomerRepositoryService } from './Repository/customer-repository/customer-repository.service';
+import { CategoryRepositoryService } from './Repository/category-repository/category-repository.service';
+import { CategoryService } from './Services/category/category.service';
+import { CustomerService } from './Services/customer/customer.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './Models/Category';
+import { Customer } from './Models/Customer';
+import { CustomerPayment } from './Models/CustomerPayment';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ResponseService } from './helpers/services/response/response.service';
+import { Currency } from './Models/Currency';
+import { Transaction } from './Models/Transaction';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      username: 'sa',
+      host: 'localhost',
+      port: 1433,
+      password: '',
+      database: 'Transactions',
+      entities: [Customer, CustomerPayment, Category, Currency, Transaction],
+      synchronize: true,
+      extra: {
+        options: {
+          encrypt: false,
+        },
+      },
+    }),
+    TypeOrmModule.forFeature([Customer, CustomerPayment, Category]),
+  ],
+  controllers: [AppController, CustomersController, CategoriesController],
+  providers: [
+    AppService,
+    CategoryService,
+    CustomerService,
+    CustomerRepositoryService,
+    CategoryRepositoryService,
+    ResponseService,
+  ],
 })
 export class AppModule {}
